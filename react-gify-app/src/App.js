748221928ac895d1042670aaf735ce93187addd2
Quincy Hicks <qdh6159@gmail.com>
2019-12-17 09:17:50 -0600

    add gifs and state

--- react-gify-app/src/App.js
+++ react-gify-app/src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SearchBar from './components/SearchBar'
+import GifList from './components/GifList'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+        gifs: [
+            {
+                id: 1,
+                url: 'http://fakeimg.pl/300/'
+            },
+            {
+                id: 2,
+                url: 'http://fakeimg.pl/300/'
+            },
+            {
+                id: 3,
+                url: 'http://fakeimg.pl/300/'
+            }
+        ]
+    }
+}
+
+  handleTermChange(term) {
+    console.log(term);
+  }
+
+  render() {
+    return (
+      <div class="text">
+        <h1>REACT - GIFY APP</h1>
+        <SearchBar onTermChange={this.handleTermChange} />
+        <GifList gifs={this.state.gifs} />
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
---
+++ react-gify-app/src/components/GifList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import GifItem from './GifItem';
+
+
+const GifList = (props) => {
+  const gifItems = props.gifs.map((image) => {
+    return <GifItem key={image.id} gif={image} />
+  });
+
+  return (
+    <ul>{gifItems}</ul>
+  );
+};
+
+export default GifList;
